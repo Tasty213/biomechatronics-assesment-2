712d7fc0e13938fef9f90dab754efbcb71dc91cb
George Sykes <gsykes537@gmail.com>
2022-12-22 17:21:34 +0000

    Add feature extraction functions

--- Data.m
+++ Data.m
@@ -4,20 +4,21 @@ classdef Data
 
     properties
         IMU_data
+        windowSize
+        windowInterval
     end
 
     methods
-        function obj = Data(pattern)
+        function obj = Data(pattern, windowSize, interval)
             arguments
                 pattern string = "IMU_Data_for_assignment/**/*.dat";
+                windowSize int16 = 1;
+                interval int16 = 1;
             end
             %Data Construct an instance of this class
-            %   Find all the data within the specified folder and
-            %   load into a cell array. Get all the columns of all the
-            %   tables and use this to get a list of all possible columns.
-            %   Inset N/A columns for any tables that are missing
-            %   columns. Concatenate all the tables together and save as
-            %   IMU_data.
+            
+            obj.windowInterval = interval;
+            obj.windowSize = windowSize;
 
             files = dir(pattern);
             columns = [];
@@ -31,20 +32,43 @@ classdef Data
             obj.IMU_data = obj.IMU_data;
             columns = unique(columns);
             for index = 1:length(obj.IMU_data)
-                data = obj.IMU_data{index};
                 for column = columns
-                    if ~any(strcmp(data.Properties.VariableNames, column)) 
-                        obj.IMU_data{index}.(column) = NaN(size(data, 1), 1);
+                    if ~any(strcmp(obj.IMU_data{index}.Properties.VariableNames, column)) 
+                        obj.IMU_data{index}.(column) = NaN(size(obj.IMU_data{index}, 1), 1);
                     end
                 end
+                obj.IMU_data{index} = obj.slidingWindows(windowSize, interval, obj.IMU_data{index});
             end
             obj.IMU_data = vertcat(obj.IMU_data{:});
+        end
+
+        function output = slidingWindows(obj, windowSize, interval, data)
+            windows = { {@(x) movmax(x, obj.windowSize), "max"}; 
+                        {@(x) movmin(x, obj.windowSize),"min"}; 
+                        {@(x) movmean(x, obj.windowSize),"mean"}; 
+                        {@(x) movstd(x, obj.windowSize),"std"};
+                      };
+            output = {};
+            for index = 1:length(windows)
+                output{index} = obj.slidingWindow(windows{index}, data);
+            end
+            output = horzcat(output{:});
+            output.Time = data.Time;
+            output.action = data.action;
+            output = output(1:interval:end, :);   
+        end
+
+        function output = slidingWindow(obj, func, data)
+            output = varfun(func{1}, data, "InputVariables", @isnumeric);
+            newNames = append(func{2},string(output.Properties.VariableNames));
+            output = renamevars(output,output.Properties.VariableNames,newNames);
+            output = removevars(output, append(func{2},"Fun_Time"));
         end
     end
 
     methods ( Static )
         function data = readDataFile(path)
-             arguments
+            arguments
                 path string;
             end
 
@@ -56,5 +80,6 @@ classdef Data
             data = removevars(data, vars);
             return
         end
+
     end
 end
--- main.m
+++ main.m
@@ -1,2 +1,3 @@
 clear();
-data = Data;
+data = Data("IMU_Data_for_assignment/**/*.dat", 400, 50);
+writetable(data.IMU_data, "output.xlsx")
